4476e06bf08fdf49fb6a01549865e74bcad4eb80
daryoush <dmehrtash@gmail.com>
2013-03-26 13:07:53 -0700

    fixed compile error

--- src/Continuations/MyInstances.hs
+++ src/Continuations/MyInstances.hs
@@ -142,10 +142,13 @@ instance (MonadCont m) => MonadCont (ReaderT r m) where
 -- WriterT
 
 newtype WriterT w m a = WriterT { runWriterT :: m (a, w) }
-instance (Monad m) => Functor (WriterT w m) where
-    fmap f m = WriterT $ do
-        ~(a, w) <- runWriterT m
-        return (f a, w)
+instance (Monoid w, Monad m) => Monad (WriterT w m) where
+    return a = WriterT $ return (a, mempty)
+    m >>= k  = WriterT $ do
+        ~(a, w)  <- runWriterT m
+        ~(b, w') <- runWriterT (k a)
+        return (b, w `mappend` w')
+    fail msg = WriterT $ fail msg
 
 
 instance (Monoid w, MonadCont m) => MonadCont (WriterT w m) where
